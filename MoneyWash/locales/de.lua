36c2511aa3c1f8116f574a90f5a9146e7a03fc14
bylickilabs <109308073+bylickilabs@users.noreply.github.com>
2024-10-18 21:15:10 +0200

    Update de.lua

--- MoneyWash/locales/de.lua
+++ MoneyWash/locales/de.lua
@@ -1,8 +1,8 @@
-Locales['de'] = {
-		['you_have_washed'] 		= 'Es wurden ~r~€ gewaschen',
+Locales['de'] = { --€ = â‚¬
+		['you_have_washed'] 		= 'Es wurden ~r~â‚¬ gewaschen',
 		['you_have_washed_waiting'] 	= 'Du hast ~r~Schwarzgeld, das gewaschen werden muss. ~s~Zeit:~b~',
 		['dirty_money'] 		= ' Schwarzgeld.',
-		['you_have_received'] 		= ' ~s~Du hast insgesamt ~g~€ erhalten',
+		['you_have_received'] 		= ' ~s~Du hast insgesamt ~g~â‚¬ erhalten',
 		['clean_money'] 		= ' Gewaschenes Geld',
 		['invalid_amount'] 		= 'Es wurde ein ungültiger Betrag eingegeben',
 		['press_menu'] 			= 'Drücke ~INPUT_CONTEXT~ um das Geld zu ~r~waschen.~s~',
